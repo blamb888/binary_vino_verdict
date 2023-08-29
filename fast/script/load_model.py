3b729e0f9e6f98e7849b1230160b68af4df4b22f
Ayata Otake <60090991+AyataOtake@users.noreply.github.com>
2023-08-29 19:25:03 +0900

    Adding to new branch

---
+++ fast/script/load_model.py
@@ -0,0 +1,27 @@
+import os
+from tensorflow import keras
+from google.cloud import storage
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+
+
+def load_model():
+    if not os.path.exists('local_model'):
+        os.makedirs('local_model')
+    client = storage.Client()
+    bucket = client.get_bucket('vv-2')l
+    blob = bucket.blob('pytorch_model.bin')
+    blob.download_to_filename('local_model/pytorch_model.bin')
+    clob = bucket.blob('config.json')
+    clob.download_to_filename('config.json')
+    try:
+        MODEL_PATH = "local_model"
+
+        # Load the model
+        model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
+        return model
+
+    except:
+        print(f"\n❌ No model found in GCS bucket")
+
+        return None
