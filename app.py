a5cf32b27a148b71abb5ce1bf24de9990a34d33e
Brandon Lamb <blamb888@gmail.com>
2023-08-31 21:48:40 +0900

    remove un imports

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import requests
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import numpy as np
-import os
 
 # Streamlit app
 st.image("https://images.unsplash.com/photo-1510812431401-41d2bd2722f3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80", caption="Wine", use_column_width=True)
